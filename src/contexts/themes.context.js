63ec333d34afa1abc466d130f40d312fc143a83c
bjordandev <b.jordan.dev@outlook.com>
2022-09-30 10:29:16 +0200

    config: set context theme with themeOne by default

---
+++ src/contexts/themes.context.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { themeOne } from "../config/themes";
+
+const ThemeContext = React.createContext(themeOne);
+
+export default ThemeContext; 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import "./reset.css";
 import './index.css';
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { themeOne } from "./config/themes";
+import ThemeContext from "./contexts/themes.context";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(<App/>);
+root.render(
+<ThemeContext.Provider value={themeOne}>
+    <App/>
+</ThemeContext.Provider>
+);
