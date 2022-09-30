f2ae9c64fda83a272703ba577237f5cbdd459214
bjordandev <b.jordan.dev@outlook.com>
2022-09-30 10:15:55 +0200

    move colors to js files

---
+++ src/config/themes.js
@@ -0,0 +1,62 @@
+const bodyStyle = getComputedStyle(document.body)
+
+const themeOne = {
+    backgroundColor: "hsl(222, 26%, 31%)",
+    backgroundScreenColor: "hsl(224, 36%, 15%)",
+    backgroundToggleColor: "hsl(223, 31%, 20%)",
+    backgroundKeypadColor: "hsl(223, 31%, 20%)",
+    buttonSymbolBackgroundColor: "hsl(30, 25%, 89%)",
+    buttonCommandBackgroundColor: "hsl(225, 21%, 49%)",
+    buttonEqualBackgroundColor: "hsl(6, 63%, 50%)",
+    buttonSymbolShadowColor: "hsl(28, 16%, 65%)",
+    buttonCommandShadowColor: "hsl(224, 28%, 35%)",
+    buttonEqualShadowColor: "hsl(6, 70%, 34%)",
+    buttonCommandTextColor: "hsl(0, 0, 100%)",
+    buttonEqualTextColor: "hsl(0, 0, 100%)",
+    buttonSymbolTextColor: "hsl(221, 14%, 31%)",
+    screenTextColor: "hsl(0, 0, 100%)",
+    buttonToggleColor: "hsl(6, 63%, 50%)" 
+};
+
+const themeTwo = {
+    backgroundColor: "hsl(0, 0%, 90%)",
+    backgroundScreenColor: "hsl(0, 0%, 93%)",
+    backgroundToggleColor: "hsl(0, 5%, 81%)",
+    backgroundKeypadColor: "hsl(0, 5%, 81%)",
+    buttonSymbolBackgroundColor: "hsl(45, 7%, 89%)",
+    buttonCommandBackgroundColor: "hsl(185, 42%, 37%)",
+    buttonEqualBackgroundColor: "hsl(25, 98%, 40%)",
+    buttonSymbolShadowColor: "hsl(35, 11%, 61%)",
+    buttonCommandShadowColor: "hsl(185, 58%, 25%)",
+    buttonEqualShadowColor: "hsl(25, 99%, 27%)",
+    buttonCommandTextColor: "hsl(0, 0, 100%)",
+    buttonEqualTextColor: "hsl(0, 0, 100%)",
+    buttonSymbolTextColor: "hsl(60, 10%, 19%)",
+    screenTextColor: "hsl(60, 10%, 19%)",
+    buttonToggleColor: "hsl(25, 98%, 40%)"
+}; 
+
+const themeThree = {
+    backgroundColor: "hsl(268, 75%, 9%)",
+    backgroundScreenColor: "hsl(268, 71%, 12%)",
+    backgroundToggleColor: "hsl(268, 71%, 12%)",
+    backgroundKeypadColor: "hsl(268, 71%, 12%)",
+    buttonSymbolBackgroundColor: "hsl(268, 47%, 21%)",
+    buttonCommandBackgroundColor: "hsl(281, 89%, 26%)",
+    buttonEqualBackgroundColor: "hsl(176, 100%, 44%)",
+    buttonSymbolShadowColor: "hsl(290, 70%, 36%)",
+    buttonCommandShadowColor: "hsl(285, 91%, 52%)",
+    buttonEqualShadowColor: "hsl(177, 92%, 70%)",
+    buttonCommandTextColor: "hsl(0, 0, 100%)",
+    buttonEqualTextColor: "hsl(198, 20%, 13%)",
+    buttonSymbolTextColor: "hsl(52, 100%, 62%)",
+    screenTextColor: "hsl(52, 100%, 62%)",
+    buttonToggleColor: "hsl(176, 100%, 44%)"
+};
+
+export {
+    themeOne, 
+    themeTwo,
+    themeThree
+}; 
+
