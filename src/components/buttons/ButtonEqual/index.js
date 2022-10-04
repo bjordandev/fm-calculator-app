07db1b4a26735eaa02a6fbd6264bea58a46d8826
bjordandev <b.jordan.dev@outlook.com>
2022-10-04 10:33:11 +0200

    enhance: make button style extensible

--- src/components/buttons/ButtonEqual/index.js
+++ src/components/buttons/ButtonEqual/index.js
@@ -2,16 +2,28 @@ import commonClasses from "../common.module.css";
 import classes from "./index.module.css";
 
 import React from "react";
+import PropTypes from "prop-types";
+
 
 function ButtonEqual(props) {
-    const { children, onClick } = props;
+    const { children, onClick, customStyles } = props;
     const className = `${commonClasses.button} ${classes.buttonEqual}`
 
     return (
-        <button className={className} onClick={onClick}>
+        <button style={customStyles} className={className} onClick={onClick}>
             {children}
         </button>
     )
+}
+
+ButtonEqual.defaultProps = {
+    children:"",
+}
+
+ButtonEqual.propTypes = {
+    children: PropTypes.string.isRequired,
+    onClick: PropTypes.func,
+    customStyles: PropTypes.object
 }
 
 export default ButtonEqual;
--- src/components/buttons/ButtonSymbol/index.js
+++ src/components/buttons/ButtonSymbol/index.js
@@ -2,16 +2,28 @@ import commonClasses from "../common.module.css";
 import classes from "./index.module.css";
 
 import React from "react";
+import PropTypes from "prop-types";
 
-function ButtonCommand(props) {
-    const { children, onClick } = props;
+
+function ButtonSymbol(props) {
+    const { children, onClick, customStyles } = props;
     const className = `${commonClasses.button} ${classes.buttonSymbol}`
 
     return (
-        <button className={className} onClick={onClick}>
+        <button style={customStyles} className={className} onClick={onClick}>
             {children}
         </button>
     )
 }
 
-export default ButtonCommand;
+ButtonSymbol.defaultProps = {
+    children:"",
+}
+
+ButtonSymbol.propTypes = {
+    children: PropTypes.string.isRequired,
+    onClick: PropTypes.func,
+    customStyles: PropTypes.object
+}
+
+export default ButtonSymbol;
