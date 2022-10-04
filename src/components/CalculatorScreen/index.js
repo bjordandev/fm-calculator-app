59c194e65a359cfdfadd2edfd32321ab00e865fb
bjordandev <b.jordan.dev@outlook.com>
2022-10-04 16:51:32 +0200

    components: create CalculatorScreen component

---
+++ src/components/CalculatorScreen/index.js
@@ -0,0 +1,15 @@
+import classes from "./index.module.css"
+import React from "react"; 
+
+function CalculatorScreen(props) {
+    const { children } = props; 
+    return (
+        <div className={classes.calculatorScreen}>
+            <p className={classes.calculatorScreenText}>
+                {children}
+            </p>
+        </div>
+    )
+}
+
+export default CalculatorScreen;
