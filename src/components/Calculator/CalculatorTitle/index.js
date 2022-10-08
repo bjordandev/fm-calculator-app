0a74f4b2af44b3bb60d9fc94899ee2ee11447f56
bjordandev <b.jordan.dev@outlook.com>
2022-10-08 10:25:32 +0200

    create calculator title

---
+++ src/components/Calculator/CalculatorTitle/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import classes from "./index.module.css"
+
+function CalculatorTitle() {
+    return (
+        <h1 className={classes.calculatorTitle}>calc</h1>
+    )
+}
+
+export default CalculatorTitle
